4f580127a73cb203b3ef132b1c9e00667ff6f458
miller777 <miller777@mail.ru>
2017-01-18 04:15:25 +0300

    Доделаны экраны регистрации и авторизации.
    Прописана логика для этих экранов.

--- app/src/main/java/ru/levelp/chatlevelup_11_12/AuthActivity.java
+++ app/src/main/java/ru/levelp/chatlevelup_11_12/AuthActivity.java
@@ -1,15 +1,56 @@
 package ru.levelp.chatlevelup_11_12;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AuthActivity extends AppCompatActivity{
+
+    private String login;
+    private String password;
+    private EditText etAuthLogin;
+    private EditText etAuthPassword;
+    private Button signInButton;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        etAuthLogin = (EditText) findViewById(R.id.et_auth_login_input);
+        etAuthPassword = (EditText) findViewById(R.id.et_auth_password_input);
+        signInButton = (Button) findViewById(R.id.btn_auth_sign_in_button);
+        signUpButton = (Button) findViewById(R.id.btn_auth_sign_up_button);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login = etAuthLogin.getText().toString();
+                password = etAuthPassword.getText().toString();
+
+                if ((login.length() == 0) || (password.length() < 8)){
+                    Toast.makeText(AuthActivity.this, getString(R.string.auth_error), Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(AuthActivity.this, R.string.auth_ok, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(AuthActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/ru/levelp/chatlevelup_11_12/RegisterActivity.java
+++ app/src/main/java/ru/levelp/chatlevelup_11_12/RegisterActivity.java
@@ -4,12 +4,46 @@ package ru.levelp.chatlevelup_11_12;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity{
+
+    private String login;
+    private String password;
+    private String confirmPassword;
+    private EditText etRegLogin;
+    private EditText etRegPassword;
+    private EditText etRegConfirmPassword;
+    private Button regButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        etRegLogin = (EditText) findViewById(R.id.et_reg_login_input);
+        etRegPassword = (EditText) findViewById(R.id.et_reg_password_input);
+        etRegConfirmPassword = (EditText) findViewById(R.id.et_reg_confirm_password_input);
+        regButton = (Button) findViewById(R.id.btn_reg_register_button);
+
+
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login = etRegLogin.getText().toString();
+                password = etRegPassword.getText().toString();
+                confirmPassword = etRegConfirmPassword.getText().toString();
+
+                if((login.length() == 0)||(password.length() < 8)||(!confirmPassword.equals(password))){
+                    Toast.makeText(RegisterActivity.this, getString(R.string.register_error), Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(RegisterActivity.this, getString(R.string.register_ok), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
