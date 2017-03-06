631564686e1046dec695def5491f28a56d5168f0
miller777 <miller777@mail.ru>
2017-03-06 13:35:47 +0300

    В первом приближении сделал экран сообщений.

---
+++ app/src/main/java/ru/levelp/chatlevelup_11_12/MessagesListActivity.java
@@ -0,0 +1,13 @@
+package ru.levelp.chatlevelup_11_12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessagesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages_list);
+    }
+}
