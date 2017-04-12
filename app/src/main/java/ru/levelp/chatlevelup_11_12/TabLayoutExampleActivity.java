917531db43257cc124b2eb9b12938d45c330c9b4
miller777 <miller777@mail.ru>
2017-04-12 03:23:26 +0300

    Начали изучать фрагменты.

---
+++ app/src/main/java/ru/levelp/chatlevelup_11_12/TabLayoutExampleActivity.java
@@ -0,0 +1,13 @@
+package ru.levelp.chatlevelup_11_12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabLayoutExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_layout_example);
+    }
+}
